cd98a9ef1e7d05eb980a05c69406a6b1ecbe9c8f
anguyen225-art <anguyen225@student.hbuhsd.edu>
2025-09-10 20:50:05 +0000

    classwork

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,34 @@
+public class Problem1
+{
+    public static void main(String[] args)
+    {
+        int num = 24;
+        System.out.println("initial number is " + num);
 
+        num = num + 1;
+        System.out.println("number is now " + num);
+
+        num += 1;
+        System.out.printf("number is now %d%n", num);
+
+        num++;
+        System.out.println("number is now " + num);
+
+        num = num + 1;
+        System.out.println("number is now " + num);
+
+        num -= 1;
+        System.out.printf("number is now %d%n",  num);
+
+        num = num - 1;
+        System.out.println("number is now " + num);
+
+        num--;
+        System.out.println("number is now " + num);
+
+        num = num - 1;
+        System.out.println("number is now " + num);
+
+
+    }
+}
