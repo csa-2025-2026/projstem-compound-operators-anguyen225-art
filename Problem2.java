9c756012e9cdb28dd041587f386cf1e4102fd73f
anguyen225-art <anguyen225@student.hbuhsd.edu>
2025-09-11 21:06:14 +0000

    classwork

--- Problem2.java
+++ Problem2.java
@@ -2,6 +2,12 @@ public class Problem2
 {
     public static void main(String[] args)
     {
-       
+       int x = 867;
+
+       System.out.println((x / 100));
+
+       System.out.println((x % 100) / 10);
+
+       System.out.println(x % 10);
     }
 }
--- Problem3.java
+++ Problem3.java
@@ -1,1 +1,15 @@
+public class Problem3
+{
+    public static void main(String[] args)
+    {
+       int x = 5678;
 
+       System.out.println(x % 10);
+
+       System.out.println((x % 100) / 10);
+
+       System.out.println((x % 1000) / 100);
+
+       System.out.println(x / 1000);
+    }
+}
